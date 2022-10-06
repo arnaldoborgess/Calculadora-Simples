b4e7f190099d83ce2ca508257288d34446d6d02f
Arnaldo Filho <arnalldofilho@gmail.com>
2022-10-05 22:06:41 -0300

    first commit

---
+++ Calculadora_Simples/Calc.cs
@@ -0,0 +1,90 @@
+﻿namespace CalculateSimple
+{
+    internal class Calculate
+    {
+        public void Add() // adição
+        {
+            Console.WriteLine("Digite o primeiro número:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o segundo número:");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"{x} + {y} = {x + y}");
+
+        }
+        public void Subtract() // subtração
+        {
+            Console.WriteLine("Digite o primeiro número:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o segundo número:");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"{x} - {y} = {x - y}");
+        }
+        public void Multiply() // Multiplicação
+        {
+            Console.WriteLine("Digite o primeiro número:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o segundo número:");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"{x} * {y} = {x * y}");
+        }
+        public void Divide() // Divisão
+        {
+            Console.WriteLine("Digite o primeiro número:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o segundo número:");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"{x} / {y} = {x / y}");
+        }
+        public void Power() // Potenciação
+        {
+            Console.WriteLine("Digite o primeiro número:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o segundo número:");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            double potential = Math.Pow(x, y);
+            Console.WriteLine($"{x}^{y} = {potential}");
+        }
+        public void Sine() // Seno
+        {
+            Console.WriteLine("Digite o primeiro número:");
+            double angle = Convert.ToInt32(Console.ReadLine());
+
+            double radians = angle * Math.PI / 180; // Conversão para radiano
+            double sine = Math.Sin(radians);
+            Console.WriteLine($"Seno de {angle}º = {Math.Round(sine, 4)}");
+        }
+
+        public void Cosine() // Coseno
+        {
+            Console.WriteLine("Digite o primeiro número:");
+            double angle = Convert.ToInt32(Console.ReadLine());
+
+            double radians = angle * Math.PI / 180;
+            double cosine = Math.Cos(radians);
+            Console.WriteLine($"Coseno de {angle}º = {Math.Round(cosine, 4)}");
+        }
+
+        public void Tangent() // Tangente
+        {
+            Console.WriteLine("Digite o primeiro número:");
+            double angle = Convert.ToInt32(Console.ReadLine());
+
+            double radians = angle * Math.PI / 180;
+            double tangent = Math.Tan(radians);
+            Console.WriteLine($"Tangente de {angle}º = {Math.Round(tangent, 4)}");
+        }
+        public void SquareRoot() // Raiz quadrada
+        {
+            Console.WriteLine("Digite o primeiro número:");
+            double x = Convert.ToInt32(Console.ReadLine());
+
+            double root = Math.Sqrt(x);
+            Console.WriteLine($"A raiz quadrada de: {x} = {Math.Round(root, 4)}");
+        }
+    }
+}
---
+++ Calculadora_Simples/Program.cs
@@ -0,0 +1,59 @@
+﻿using Calculadora_Simples;
+using CalculateSimple;
+using System.ComponentModel;
+
+Calculate calc = new Calculate();
+
+int calcular;
+
+Console.WriteLine("1 - Somar, 2 - Subtrair, 3 - Multiplicar, 4 - Dividir, " +
+    "5 - Potenciação, 6 - Seno, 7 - Coseno, 8 - Tangente, 9 - Raiz Quadrada");
+
+Console.WriteLine("Escolha uma Opção.");
+
+calcular = int.Parse(Console.ReadLine());
+
+switch (calcular)
+{
+
+    case 1:
+        Console.WriteLine("SOMA");
+        calc.Add();
+        break;
+    case 2:
+        Console.WriteLine("SUBTRAÇÃO");
+        calc.Subtract();
+        break;
+    case 3:
+        Console.WriteLine("MULTIPLICAÇÃO");
+        calc.Multiply();
+        break;
+    case 4:
+        Console.WriteLine("DIVISÃO");
+        calc.Divide();
+        break;
+    case 5:
+        Console.WriteLine("POTENCIAÇÃO");
+        calc.Power();
+        break;
+    case 6:
+        Console.WriteLine("SENO");
+        calc.Sine();
+        break;
+    case 7:
+        Console.WriteLine("COSENO");
+        calc.Cosine();
+        break;
+    case 8:
+        Console.WriteLine("TANGENTE");
+        calc.Tangent();
+        break;
+    case 9:
+        Console.WriteLine("RAIZ QUADRADA");
+        calc.SquareRoot();
+        break;
+    default:
+        Console.WriteLine("Opção inválida, tente novamente!");
+        break;
+
+}
